348d0aa189606c4837b573aeb7cc20d11369d55c
root <root@DESKTOP-598GE48.localdomain>
2023-10-16 14:47:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Clean the input word by removing spaces and special characters and convert it to lowercase
+  const cleanedWord = word.replace(/[^a-zA-Z]/g, '').toLowerCase();
+  
+  // Initialize two pointers, one starting from the beginning and the other from the end of the cleaned word
+  let start = 0;
+  let end = cleanedWord.length - 1;
+
+  // While the start pointer is less than the end pointer, do the following:
+  while (start < end) {
+    // If the characters at the start and end do not match, return false
+    if (cleanedWord[start] !== cleanedWord[end]) {
+      return false;
+    }
+
+    // Move the start pointer forward and the end pointer backward
+    start++;
+    end--;
+  }
+
+  // If the loop completes without returning false, it means the word is a palindrome, so return true
+  return true;
 }
 
-/* 
-  Add your pseudocode here
-*/
+// Pseudocode:
+// 1. Remove spaces and special characters from the input word.
+// 2. Convert the cleaned word to lowercase.
+// 3. Initialize two pointers (start and end) at the beginning and end of the cleaned word.
+// 4. While the start pointer is less than the end pointer, compare characters.
+// 5. If any characters don't match, return false (it's not a palindrome).
+// 6. If the loop completes without returning false, return true (it's a palindrome).
 
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
